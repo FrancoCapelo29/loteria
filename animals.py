929a9f54f3aabfa5099aef8860e938f5335f7ac1
Andres aucay <maaucay@sudamericano.edu.ec>
2023-07-25 12:04:37 -0500

    animales ganadores

---
+++ animals.py
@@ -0,0 +1,17 @@
+def mascotas_premiadas(mascota_ingresada):
+    mascotas_ganadoras = {
+        "ornitorinco": "es la mascota ganadora",
+        "pato": "estás en el segundo lugar",
+        "lagarto" : "tercer lugar"
+    }
+
+    premio = mascotas_ganadoras.get(mascota_ingresada)
+    if premio:
+        print(f"{mascota_ingresada}, {premio}.")
+    else:
+        print("Buena suerte para la próxima vez!")
+
+nombre_mascota = input("Ingresa el nombre de una mascota: ")
+
+
+mascotas_premiadas(nombre_mascota.lower())  
